37f841bd06ae4058341ae805b4e1d76117f110b4
sonlevu59 <svu23@iastate.edu>
2025-11-06 22:38:07 -0600

    temp solution for result backend celery set to none

--- testpas/settings.py
+++ testpas/settings.py
@@ -144,7 +144,8 @@ app.conf.broker_transport_options = {
 }
  
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
+# CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
+CELERY_RESULT_BACKEND = None
 # Define how many seconds represent one simulated "day"
 
 # Email settings
