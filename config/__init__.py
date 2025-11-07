1e6454ef8b85e15d24db2059562ac859ee594360
sonlevu59 <svu23@iastate.edu>
2025-11-06 23:00:07 -0600

    update init file round 3

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testpas.settings')
 
 app = Celery('config')
 
