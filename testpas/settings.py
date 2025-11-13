ad8dba1846c0a04ecf02f695bb2484c4bf660a22
HoldenSmithia <holden.smith.ia@icloud.com>
2025-11-12 21:15:57 -0600

    Added postres functionality

--- testpas/settings.py
+++ testpas/settings.py
@@ -16,6 +16,19 @@ import os
 import environ
 from dotenv import load_dotenv
 load_dotenv()
+
+import dj_database_url
+import os
+
+# Use DATABASE_URL if provided (Render PostgreSQL), else default to SQLite
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env()
@@ -45,18 +58,7 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': os.getenv('DB_NAME'),
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': '5432',
-    }
-}
+
 
 # # # SendGrid configuration
 # SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
